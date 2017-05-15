465249a18c6ba8d4a76a7e7ef16019a70014ff5d
VladYeqorov <dreamhacker308@gmail.com>
2017-05-15 09:43:34 +0400

    First Commit

---
+++ Functions.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+
+void input()
+{
+	int key;
+
+	while (true)
+	{
+		key = getch();
+
+		if (key == 97)
+		{
+			cout << "o-  A ";
+		}
+		else if (key == 98)
+		{
+			cout << "-ooo  B ";
+		}
+		else if (key == 99)
+		{
+			cout << "-o-o  C ";
+		}
+		else if (key == 100)
+		{
+			cout << "-oo  D ";
+		}
+		else if (key == 101)
+		{
+			cout << "o  E ";
+		}
+		else if (key == 102)
+		{
+			cout << "oo-o  F ";
+		}
+		else if (key == 103)
+		{
+			cout << "--o  G ";
+		}
+		else if (key == 104)
+		{
+			cout << "oooo  H ";
+		}
+		else if (key == 105)
+		{
+			cout << "oo   I ";
+		}
+		else if (key == 106)
+		{
+			cout << "o---  J ";
+		}
+		else if (key == 107)
+		{
+			cout << "-o-  K ";
+		}
+		else if (key == 108)
+		{
+			cout << "o-oo  L ";
+		}
+		else if (key == 109)
+		{
+			cout << "--  M ";
+		}
+		else if (key == 110)
+		{
+			cout << "-o  N ";
+		}
+		else if (key == 111)
+		{
+			cout << "---  O ";
+		}
+		else if (key == 112)
+		{
+			cout << "o--o  P ";
+		}
+		else if (key == 113)
+		{
+			cout << "--o-  Q ";
+		}
+		else if (key == 114)
+		{
+			cout << "o-o  R ";
+		}
+		else if (key == 115)
+		{
+			cout << "ooo   S ";
+		}
+		else if (key == 116)
+		{
+			cout << "-   T ";
+		}
+		else if (key == 117)
+		{
+			cout << "oo-  U ";
+		}
+		else if (key == 118)
+		{
+			cout << "ooo-   V ";
+		}
+		else if (key == 119)
+		{
+			cout << "o--   W ";
+		}
+		else if (key == 120)
+		{
+			cout << "-oo-  X ";
+		}
+		else if (key == 121)
+		{
+			cout << "-o--   Y ";
+		}
+		else if (key == 122)
+		{
+			cout << "--oo   Z ";
+		}
+		else if (key == 48)
+		{
+			cout << "-----   0 ";
+		}
+		else if (key == 49)
+		{
+			cout << "o----    1 ";
+		}
+		else if (key == 50)
+		{
+			cout << "oo---   2 ";
+		}
+		else if (key == 51)
+		{
+			cout << "ooo--   3 ";
+		}
+		else if (key == 52)
+		{
+			cout << "oooo-   4 ";
+		}
+		else if (key == 53)
+		{
+			cout << "ooooo   5 ";
+		}
+		else if (key == 54)
+		{
+			cout << "-oooo   6 ";
+		}
+		else if (key == 55)
+		{
+			cout << "--ooo   7 ";
+		}
+		else if (key == 56)
+		{
+			cout << "---oo    8 ";
+		}
+		else if (key == 57)
+		{
+			cout << "----o    9 ";
+		}
+	}
+}
